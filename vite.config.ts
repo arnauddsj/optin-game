4d5be980643b4847cb5622b3a18e83bcf07651fd
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2024-09-26 13:32:09 +0200

    up ocnfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite'
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import svgLoader from 'vite-svg-loader'
 
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -15,8 +16,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
-  plugins: [vue()],
-  base: './',
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
