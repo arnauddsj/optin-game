33cde3744ee1155aa05948e3d726aa6cae5453bf
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2024-09-26 13:12:00 +0200

    up

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    sourcemap: true,
+  },
   plugins: [vue()],
   base: './',
   resolve: {
