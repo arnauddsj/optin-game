0039ba9992bb1cb57b721eb1804251207e7a5f34
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2024-09-26 13:46:08 +0200

    Update SplashScreen.vue

--- src/views/SplashScreen.vue
+++ src/views/SplashScreen.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const startGame = () => {
-  router.push('/game')
+  router.push('/optin')
 }
 </script>
 
@@ -12,7 +12,7 @@ const startGame = () => {
   <div class="flex flex-col items-center justify-center h-screen bg-sky-800 text-slate-50">
     <h1 class="text-4xl font-bold mb-8">Welcome to VolksWagen History Game!</h1>
     <button @click="startGame" class="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">
-      Start Game
+      Test optin
     </button>
   </div>
 </template>
