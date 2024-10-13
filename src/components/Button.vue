7302a85060961e98fb0549633ba05a17c8a17f2c
Romain Bianzani <romain.bianzani@bksystemes.local>
2024-10-13 22:26:11 +0200

    game 1 readjust

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -21,7 +21,7 @@ const handleClick = () => {
 
 <template>
   <div>
-    <button @click="handleClick" class="bg-vw-light text-white text-1xl font-medium py-1 px-2 whitespace-nowrap outline-none inline-block mx-auto">
+    <button @click="handleClick" class="bg-vw-light text-white text-1xl font-medium py-1 px-2 whitespace-nowrap outline-none inline-block mx-auto" style="min-height: 2rem!important;">
       {{ cta }}
     </button>
   </div>
