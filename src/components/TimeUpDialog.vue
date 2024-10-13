6d3e3e41d037ce1c65f0179389c7ca951f979455
Romain Bianzani <romain.bianzani@bksystemes.local>
2024-10-13 22:20:55 +0200

    game 1 readjust

--- src/components/TimeUpDialog.vue
+++ src/components/TimeUpDialog.vue
@@ -30,10 +30,10 @@ const handleQuit = () => {
       <DialogOverlay class="fixed inset-0 z-50 bg-black/50" />
       <DialogContent class="fixed inset-0 z-50 flex items-center justify-center">
         <div class="flex flex-col gap-5 bg-white py-10 px-5 shadow-lg text-center text-vw-dark w-[80%] max-w-md">
-          <DialogTitle class="text-3xl font-bold">
+          <DialogTitle class="font-bold" style="line-height: 1.25rem;">
             Malheureusement le temps est écoulé !
           </DialogTitle>
-          <DialogDescription class="text-2xl">
+          <DialogDescription>
             Passez à l’épreuve suivante.
           </DialogDescription>
           <div class="flex flex-col justify-center items-center gap-2">
