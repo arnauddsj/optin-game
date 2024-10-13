289e1dca7d1805294f5b35fa38cbcd360dfefc5b
Romain Bianzani <romain.bianzani@bksystemes.local>
2024-10-13 21:46:11 +0200

    resize button text

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -21,7 +21,7 @@ const handleClick = () => {
 
 <template>
   <div class="inline-block">
-    <button @click="handleClick" class="bg-vw-light text-white text-2xl font-medium py-1 px-8 whitespace-nowrap outline-none">
+    <button @click="handleClick" class="bg-vw-light text-white text-1xl font-medium py-1 px-8 whitespace-nowrap outline-none">
       {{ cta }}
     </button>
   </div>
