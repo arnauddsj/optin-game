57bbe89a14455ebfe0389bf35eeef032d4fcdc81
Romain Bianzani <romain.bianzani@bksystemes.local>
2024-10-14 07:39:07 +0200

    home rebuild

--- src/components/TimeUpDialog.vue
+++ src/components/TimeUpDialog.vue
@@ -30,10 +30,10 @@ const handleQuit = () => {
       <DialogOverlay class="fixed inset-0 z-50 bg-black/50" />
       <DialogContent class="fixed inset-0 z-50 flex items-center justify-center">
         <div class="flex flex-col gap-4 bg-white py-10 px-4 shadow-lg text-center text-vw-dark w-[80%] max-w-md">
-          <DialogTitle class="font-bold" style="line-height: 1.25rem;">
+          <DialogTitle class="font-bold" style="line-height: 1.25rem; font-size: 0.85rem;">
             Malheureusement le temps est écoulé !
           </DialogTitle>
-          <DialogDescription>
+          <DialogDescription style="line-height: 0.9rem; font-size: 0.7rem;">
             Passez à l’épreuve suivante.
           </DialogDescription>
           <div class="flex flex-col justify-center items-center gap-2">
